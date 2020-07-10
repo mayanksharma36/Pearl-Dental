96fb3ba8538071ba61d56917267100edf320a83f
mayanksharma36 <mswork36@gmail.com>
2020-07-10 20:11:15 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function Toggle(){
+	var header_menu=document.querySelector(".h-menu");
+	  if(header_menu.style.display=='block'){
+		  
+		  header_menu.style.display='none';
+	  }
+	  else{
+		    header_menu.style.display='block';
+	  }
+	
+}
